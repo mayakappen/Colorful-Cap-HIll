ad6fe404f83d4e9bdbb336fda50f57a77fe439f2
Maya Kappen <mayakappen@gmail.com>
2024-07-02 16:13:59 -0600

    adjust colors

--- script.js
+++ script.js
@@ -1,11 +1,16 @@
 let navbar = document.querySelector('.header .navbar');
 let menuBtn = document.querySelector('#menu-btn');
+let closeBtn = document.querySelector('#close-navbar');
 
 menuBtn.onclick = () => {
     navbar.classList.toggle('active');
 };
 
 window.onscroll = () => {
+    navbar.classList.remove('active');
+};
+
+closeBtn.onclick = () => {
     navbar.classList.remove('active');
 };
 
